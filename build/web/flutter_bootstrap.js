259e967ba362074d18943891797451bd2813417f
vtry <vtry@example.com>
2026-01-16 06:09:32 +0500

    final: production build with cloudinary and try-on fixes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "149585438"
+    serviceWorkerVersion: "671960202"
   }
 });
